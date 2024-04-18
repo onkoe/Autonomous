6583abe1d26f1ee5acf47ff021c28c593367ddf8
Barrett <onkoe@protonmail.com>
2024-04-17 19:24:59 -0500

    fix: `ar.py` Example (so MyPy shuts up)

--- examples/ar.py
+++ examples/ar.py
@@ -1,12 +1,10 @@
 from time import sleep
-
-import os
 from libs import ARTracker
 
-tracker = ARTracker.ARTracker(['/dev/video1'], write=False, useYOLO = False) #ARTracker requires a list of camera files
+tracker = ARTracker.ARTracker([0], write=False) #ARTracker requires a list of camera files
 
 while True:
-    tracker.findMarker(1)#, id2 = 1)
-    print('Distance (in cm): ', tracker.distanceToMarker)
-    print('Angle: ', tracker.angleToMarker)
+    tracker.find_marker(1)#, id2 = 1)
+    print('Distance (in cm): ', tracker.distance_to_marker)
+    print('Angle: ', tracker.angle_to_marker)
     sleep(.5)
