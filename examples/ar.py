5ce09cc67036519ee6b552302a4b8c2f5aff121d
Barrett <onkoe@protonmail.com>
2024-05-16 18:02:37 -0500

    fix(examples/ar): Handle case where marker isn't found

--- examples/ar.py
+++ examples/ar.py
@@ -1,10 +1,16 @@
 from time import sleep
 from libs import aruco_tracker
+from loguru import logger
 
-tracker = aruco_tracker.ARTracker([0], write=False) #ARTracker requires a list of camera files
+tracker = aruco_tracker.ARTracker([0], write=False) # ARTracker requires a list of camera IDs
+marker = 1 # change this ARUCO identifier if you need to :)
 
 while True:
-    tracker.find_marker(1)#, id2 = 1)
-    print('Distance (in cm): ', tracker.distance_to_marker)
-    print('Angle: ', tracker.angle_to_marker)
-    sleep(.5)
+    found = tracker.find_marker(marker)
+    
+    if found:
+        print('Distance (in cm): ', tracker.distance_to_marker)
+        print('Angle: ', tracker.angle_to_marker)
+    else:
+        logger.error(f"Didn't find marker {marker}")
+    sleep(0.5)
